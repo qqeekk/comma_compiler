8912bc5902be742c515543c2d06902cb3cd4da3a
Alexander Drozdov <drozdov.alexander.98@gmail.com>
2019-02-10 18:11:12 +0700

    Initial lexer (not tested)

---
+++ Comma/ErrorLogger.fs
@@ -0,0 +1,45 @@
+﻿namespace Comma
+
+module Errors =
+    type ErrorMessage = | ErrorMessage of string
+    type WarningMessage = | WarningMessage of string
+
+    let integerOverflow = 
+        ErrorMessage "This number is outside the allowable range for 32-bit signed integers"
+    
+    let unknownIdentifier = 
+        ErrorMessage "Invalid symbol or identifier"
+    
+    let invalidFloat =
+        ErrorMessage "Invalid floating point number"
+
+    let unmatchedDoubleQuote = 
+        ErrorMessage "Unmatched '\"'"
+
+module ErrorLogger =
+    open System
+    open Errors
+
+    type Position = int * int
+
+    let errorColor = ConsoleColor.Red
+    let warningColor = ConsoleColor.Yellow
+    
+    let printWithColor color (msg : string) =
+        let curColor = Console.ForegroundColor
+        Console.ForegroundColor <- color
+        Console.WriteLine msg
+        Console.ForegroundColor <- curColor
+
+    let logLexErrorWithToken (pos: Position) (tok: string) (ErrorMessage msg) = 
+        printWithColor errorColor (sprintf "Lexing error at %A : %s %A" pos msg tok)
+
+    let logLexError (startPos: Position) (endPos: Position) (ErrorMessage msg) = 
+        printWithColor errorColor (sprintf "Lexing error at %A - %A : %s" startPos endPos msg)
+
+    let logLexWarningWithToken (pos: Position) (tok: string) (WarningMessage msg) =
+        printWithColor warningColor (sprintf "Lexing warning at %A : %s %A" pos msg tok)
+        
+    let logLexWarning (startPos: Position) (endPos: Position) (WarningMessage msg) =
+        printWithColor warningColor (sprintf "Lexing warning at %A - %A : %s" startPos endPos msg)
+
--- Comma/Program.fs
+++ Comma/Program.fs
@@ -1,8 +1,58 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open Microsoft.FSharp.Text.Lexing
+open System.Text
+open System.IO
+open System.Diagnostics
+open Parser
 
-open System
+let fileExtensions = [| ".cmm" |]
+let encoding = Encoding.UTF8
+
+let safeOpenFile filename : Result<FileStream, string> = 
+    match File.Exists filename with
+    | true -> 
+        match Array.contains (Path.GetExtension filename) fileExtensions with
+        | true -> 
+            try 
+                Ok (File.OpenRead filename)
+            with _ -> 
+                Error ("Cannot open a file: " + filename)
+        | false -> 
+            Error ("Wrong file format: " + filename)
+    | false ->
+        Error ("File does not exist: " + filename)
+
+
+let iterateTokens callback lexbuf =
+    // needs to be tail-recursive
+    let rec loop () =
+        match Lexer.tokenize lexbuf with 
+        | Eof -> () 
+        | x -> do callback lexbuf.EndPos x; loop ()
+    
+    do loop ()
+
+
+let compileFromLexbuf lexbuf =
+    let inline print (p : Position) tok =
+        Debug.Print (sprintf "token %A: %A\n" (p.Line + 1, p.Column) tok)
+
+    do iterateTokens print lexbuf
+
+let compileText (text: string) =
+    LexBuffer<_>.FromBytes (encoding.GetBytes text) |> compileFromLexbuf
+    
+
+let compileFile filename =
+    match (safeOpenFile filename) with
+    | Ok file ->
+        use stream = new BinaryReader(file, encoding)
+        do compileFromLexbuf (LexBuffer<_>.FromBinaryReader stream)        
+    | Error text -> 
+        printfn "%s" text
+
 
 [<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+let main = function
+    | [| "-f" ; filename |] -> compileFile filename; 0
+    | [| text |] -> compileText text; 0
+    | _ -> (); 1
