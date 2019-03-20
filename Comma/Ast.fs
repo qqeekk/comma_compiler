b5b467a947fe9f54baa5ce54feac74dbdb412519
Alexander Drozdov <drozdov.alexander.98@gmail.com>
2019-03-20 23:09:01 +0700

    Primitive parsing into ast, started to implement error reporting (yet not building)

---
+++ Comma/Ast.fs
@@ -0,0 +1,53 @@
+﻿module Ast
+open Microsoft.FSharp.Text.Parsing
+
+type Param = string * TypeId
+and FunDecl = { name : string; _params : Param list; retType : TypeId; body : Stmt list }
+and TypeDecl = { name : string; fields : Param list}
+and Decl =
+    | FunDecl of FunDecl
+    | TypeDecl of TypeDecl
+and TypeId = 
+    | Single of string
+    | Array of string
+and Stmt =
+    | Empty
+    | Expression of Expr
+    | VarDecl of Param * Expr
+    | Loop of LoopStmt
+    | IfElse of Expr * Stmt * Stmt
+    | Label of string
+    | GoTo of string
+    | Return of Expr
+    | Break 
+    | Continue
+    | StmtBlock of Stmt list
+and LoopStmt =
+    | While of Expr * Stmt
+    | DoWhile of Stmt * Expr
+and Expr = 
+    | Integer of int
+    | Boolean of bool
+    | Float of float
+    | String of string
+    | Identifier of string
+    | StructInit of string * (string * Expr) list
+    | FieldAccess of Expr * string
+    | IndexOrArrayInit of Expr * Expr
+    | Neg of Expr
+    | UMinus of Expr
+    | FuncApp of string * (Expr list)
+    | Assign of Expr * Expr
+    | Add of Expr * Expr
+    | Sub of Expr * Expr
+    | Mul of Expr * Expr
+    | Div of Expr * Expr
+    | Or of Expr * Expr
+    | And of Expr * Expr
+    | Equals of Expr * Expr
+    | Greater of Expr * Expr
+    | GreaterEq of Expr * Expr
+
+exception SyntaxError of ParseErrorContext<obj>
+
+let internal tokenPos (parseState: IParseState) : int -> _ = parseState.InputRange
--- Comma/Program.fs
+++ Comma/Program.fs
@@ -2,47 +2,53 @@
 open System.Text
 open System.IO
 open Parser
-open Comma.Errors
 open Comma.ErrorLogger
-open Comma
-open System
+open System.Runtime.Serialization
+open System.Xml
+open MBrace.FsPickler
+open MBrace.FsPickler.Combinators
 
 let fileExtensions = [| ".cmm" |]
 let encoding = Encoding.UTF8
+
+
+let serializeToXML o =
+    let xmlSerializer = FsPickler.CreateXmlSerializer(indent = true)
+    use writer = new StringWriter ()
+    xmlSerializer.Serialize (writer, o)
+    writer.ToString()
+   
 
 let safeOpenFile filename : Result<FileStream, string> = 
     match File.Exists filename with
     | true -> 
         match Array.contains (Path.GetExtension filename) fileExtensions with
         | true -> 
-            try 
-                Ok (File.OpenRead filename)
-            with _ -> 
-                Error ("Cannot open a file: " + filename)
-        | false -> 
-            Error ("Wrong file format: " + filename)
-    | false ->
-        Error ("File does not exist: " + filename)
+            try Ok (File.OpenRead filename)
+            with _ -> Error ("Cannot open a file: " + filename)
+        | false -> Error ("Wrong file format: " + filename)
+    | false -> Error ("File does not exist: " + filename)
     
+
 let compileFromLexbuf (lexbuf:LexBuffer<_>) =
     use file = File.CreateText "lex.output.txt"
     let logger = (consoleLogger >=> (* debugLogger >=> *) fileLogger file)
 
-    let rec iterate () =
-        match Lexer.getToken logger lexbuf with 
-        | Eof _ -> () 
-        | _   -> iterate ()
+    //iterate ()
+    let parsed = Parser.program (Lexer.getToken logger) lexbuf
+    info logger (string parsed)
+    //info logger (serializeToXML parsed)
 
-    iterate ()
 
 let compileText (text: string) =
     LexBuffer<_>.FromBytes (encoding.GetBytes text) |> compileFromLexbuf
+
 
 let compileFile filename =
     match (safeOpenFile filename) with
     | Ok file    ->
         use stream = new BinaryReader(file, encoding)
-        do compileFromLexbuf (LexBuffer<_>.FromBinaryReader stream)        
+        compileFromLexbuf (LexBuffer<_>.FromBinaryReader stream)
     | Error text -> 
         printfn "%s" text
 
