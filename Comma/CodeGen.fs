170b592b676f2a8abc3bf61b6cc7ea250ec2bf54
Alexander Drozdov <drozdov.alexander.98@gmail.com>
2019-06-10 06:21:42 +0700

    qs now works

--- Comma/CodeGen.fs
+++ Comma/CodeGen.fs
@@ -4,7 +4,7 @@ open LCode
 
 let constant = """
 @print_str.ptr = private constant [4 x i8] c"%s\0A\00"
-@print_bln.ptr = private constant [4 x i8] c"%i\0A\00"
+@print_int.ptr = private constant [4 x i8] c"%d\0A\00"
 @print_flt.ptr = private constant [4 x i8] c"%f\0A\00"
 """
 
@@ -19,29 +19,29 @@ define i32 @main(i32, i8**) {
 
 define void @print_str (i32* %ret.value, i8* %str) {
     %print_str.ptr = getelementptr inbounds [4 x i8], [4 x i8]* @print_str.ptr, i32 0, i32 0
-    %ret = call i32 (i8*, ...) @printf(i8* print_str.ptr, i8* %str)
-    strore i32 %ret, i32* %ret.value
+    %ret = call i32 (i8*, ...) @printf(i8* %print_str.ptr, i8* %str)
+    store i32 %ret, i32* %ret.value
     ret void 
 }
 
 define void @print_int (i32* %ret.value, i32 %i) {
     %print_int.ptr = getelementptr inbounds [4 x i8], [4 x i8]* @print_int.ptr, i32 0, i32 0
-    %ret = call i32 (i8*, ...) @printf(i8* print_int.ptr, i32 %i)
-    strore i32 %ret, i32* %ret.value
+    %ret = call i32 (i8*, ...) @printf(i8* %print_int.ptr, i32 %i)
+    store i32 %ret, i32* %ret.value
     ret void 
 }
 
 define void @print_flt (i32* %ret.value, double %d) {
     %print_flt.ptr = getelementptr inbounds [4 x i8], [4 x i8]* @print_flt.ptr, i32 0, i32 0
-    %ret = call i32 (i8*, ...) @printf(i8* print_flt.ptr, double %d)
-    strore i32 %ret, i32* %ret.value
+    %ret = call i32 (i8*, ...) @printf(i8* %print_flt.ptr, double %d)
+    store i32 %ret, i32* %ret.value
     ret void 
 }
 
 define void @print_bln (i32* %ret.value, i1 %b) {
     %print_bln.ptr = getelementptr inbounds [4 x i8], [4 x i8]* @print_int.ptr, i32 0, i32 0
-    %ret = call i32 (i8*, ...) @printf(i8* print_bln.ptr, i1 %b)
-    strore i32 %ret, i32* %ret.value
+    %ret = call i32 (i8*, ...) @printf(i8* %print_bln.ptr, i1 %b)
+    store i32 %ret, i32* %ret.value
     ret void 
 }
 
